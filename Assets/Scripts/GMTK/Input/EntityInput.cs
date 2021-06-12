66ec313d2aeba46bb3d3965d1230676228ac83fd
Bruno Silva Freire <brunorbsf@gmail.com>
2021-06-12 16:22:12 -0400

    Intro cutscene

--- Assets/Scripts/GMTK/Input/EntityInput.cs
+++ Assets/Scripts/GMTK/Input/EntityInput.cs
@@ -1,21 +1,41 @@
-﻿    using Lunari.Tsuki.Entities;
+﻿using Lunari.Tsuki.Entities;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace GMTK.Input {
     public class EntityInput : Trait {
         public InputSource source;
+        public bool zeroed;
+
         [ShowInInspector]
         public Vector3 MousePosition { get; private set; }
+
         [ShowInInspector]
         public bool MouseDown { get; private set; }
+
         [ShowInInspector]
         public bool MouseUp { get; private set; }
 
+        public bool Zeroed {
+            get => zeroed;
+            set {
+                zeroed = value;
+                if (value) {
+                    MouseDown = false;
+                    MouseUp = false;       
+                }
+            }
+        }
+
         private void Update() {
             MousePosition = source.GetMousePosition();
-            MouseDown = source.GetMouseDown();
-            MouseUp = source.GetMouseUp();
+            if (Zeroed) {
+                MouseDown = false;
+                MouseUp = false;
+            } else {
+                MouseDown = source.GetMouseDown();
+                MouseUp = source.GetMouseUp();
+            }
         }
     }
 }
---
+++ Assets/Scripts/GMTK/Master/GameInputPlayableAsset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GMTK.Input;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GMTK.Master {
+    [Serializable]
+    public class GameInputPlayableAsset : PlayableAsset {
+        
+        public ExposedReference<EntityInput> input;
+        public bool valid;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
+            var playable = ScriptPlayable<GameInputPlayable>.Create(graph);
+            var inputPlayable = playable.GetBehaviour();
+            inputPlayable.input = input.Resolve(graph.GetResolver());
+            inputPlayable.valid = valid;
+            return playable;
+        }
+    }
+}
