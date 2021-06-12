859f05d0261df58b5d1a4487c2f01e9d432ea86a
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-06-12 16:47:08 -0300

    -Improve neutro spawn directions

--- Assets/Scripts/GMTK/Game/Traits/HappyAtom.cs
+++ Assets/Scripts/GMTK/Game/Traits/HappyAtom.cs
@@ -15,41 +15,46 @@ namespace GMTK.Game.Traits {
         [Range(0f, 1f)]
         public float additionalNeutronChance = 0.5f;
         public int baseNeutronCount = 3;
-        public int angryAtomsCount = 2;
+        private const int AngryAtomsCount = 2;
+
+        [Range(0f, 90f)]
+        public float neutronArc = 30f;
+        public float neutronForce = 4f;
+        public float angryAtomForce = 4f;
 
         private Vector3 Vector3FromAngle(float angle) {
             return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
         }
 
         private float AngleFromIndex(int index, int totalIndexes) {
-            return (float)index / totalIndexes * Mathf.PI * 2;
+            return Mathf.Deg2Rad * Mathf.Lerp(-neutronArc, neutronArc, (float)index / totalIndexes);
         }
         
         protected override void OnCollisionWithNeutron(Collision collision) {
+            var normal = collision.contacts[0].normal;
+            var rightToNormal = Vector3.Cross(normal, Vector3.forward);
             
             var neutronCount = baseNeutronCount;
             if (Random.value < additionalNeutronChance) {
                 neutronCount++;
             }
             
-            int totalChildren = neutronCount + angryAtomsCount;
             int childIndex = 0;
-            float angleOffset = Vector3.Angle(collision.contacts[0].normal, Vector3.right);
-            float impulse = collision.impulse.magnitude;
+            float angleOffset = Mathf.Atan2(normal.y, normal.x);
 
             for (int i = 0; i < neutronCount; i++) {
-                var direction = Vector3FromAngle(AngleFromIndex(childIndex, totalChildren) + angleOffset);
+                var direction = Vector3FromAngle(AngleFromIndex(childIndex, Mathf.Max(neutronCount - 1, 1)) + angleOffset);
                 var neutron = neutronPrefab.Clone(transform.position + direction * 2);
-                neutron.rigidbody.AddForce(direction * impulse, ForceMode.Impulse);
+                neutron.rigidbody.AddForce(direction * neutronForce, ForceMode.Impulse);
                 childIndex++;
             }
 
-            for (int i = 0; i < angryAtomsCount; i++) {
-                var direction = Vector3FromAngle(AngleFromIndex(childIndex, totalChildren) + angleOffset);
+            for (int i = 0; i < AngryAtomsCount; i++) {
+                var direction = i % 2 == 0 ? rightToNormal : -rightToNormal;
                 var angryAtoms = angryAtom.Clone(transform.position + direction * 2);
-                angryAtoms.rigidbody.AddForce(direction * impulse, ForceMode.Impulse);
-                childIndex++;
+                angryAtoms.rigidbody.AddForce(direction * angryAtomForce, ForceMode.Impulse);
             }
+            
             Destroy(gameObject);
             Destroy(collision.rigidbody.gameObject);
         }
