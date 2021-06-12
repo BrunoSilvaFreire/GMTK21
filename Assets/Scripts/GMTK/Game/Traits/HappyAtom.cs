aa128e8485401ee9ad8d53320ff59d7fde8c4f68
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-06-12 13:13:14 -0300

    -Add chance to spawn additional neutron

--- Assets/Scripts/GMTK/Game/Traits/HappyAtom.cs
+++ Assets/Scripts/GMTK/Game/Traits/HappyAtom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Lunari.Tsuki.Runtime;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GMTK.Game.Traits {
     public class HappyAtom : Atom {
@@ -8,8 +9,10 @@ namespace GMTK.Game.Traits {
         [SerializeField] private Neutron neutronPrefab;
         [SerializeField] private AngryAtom angryAtom;
 
-        public int neutronsReleasedOnCollision = 3;
-        public int angryAtomsReleasedOnCollision = 2;
+        [Range(0f, 1f)]
+        public float additionalNeutronChance = 0.5f;
+        public int baseNeutronCount = 3;
+        public int angryAtomsCount = 2;
 
         private Vector3 Vector3FromAngle(float angle) {
             return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
@@ -21,19 +24,24 @@ namespace GMTK.Game.Traits {
         
         protected override void OnCollisionWithNeutron(Collision collision) {
             
-            int totalChildren = neutronsReleasedOnCollision + angryAtomsReleasedOnCollision;
+            var neutronCount = baseNeutronCount;
+            if (Random.value < additionalNeutronChance) {
+                neutronCount++;
+            }
+            
+            int totalChildren = neutronCount + angryAtomsCount;
             int childIndex = 0;
             float angleOffset = Vector3.Angle(collision.contacts[0].normal, Vector3.right);
             float impulse = collision.impulse.magnitude;
 
-            for (int i = 0; i < neutronsReleasedOnCollision; i++) {
+            for (int i = 0; i < neutronCount; i++) {
                 var direction = Vector3FromAngle(AngleFromIndex(childIndex, totalChildren) + angleOffset);
                 var neutron = neutronPrefab.Clone(transform.position + direction * 4);
                 neutron.rigidbody.AddForce(direction * impulse, ForceMode.Impulse);
                 childIndex++;
             }
 
-            for (int i = 0; i < angryAtomsReleasedOnCollision; i++) {
+            for (int i = 0; i < angryAtomsCount; i++) {
                 var direction = Vector3FromAngle(AngleFromIndex(childIndex, totalChildren) + angleOffset);
                 var angryAtoms = angryAtom.Clone(transform.position + direction * 4);
                 angryAtoms.rigidbody.AddForce(direction * impulse, ForceMode.Impulse);
