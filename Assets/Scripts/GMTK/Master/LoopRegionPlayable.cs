d0de8c56e4422005424c4e93db5dc7f2cf112e75
Bruno Silva Freire <brunorbsf@gmail.com>
2021-06-12 17:53:13 -0400

    > Music yay

---
+++ Assets/Scripts/GMTK/Master/LoopRegionPlayable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GMTK.Master {
+    public class LoopRegionPlayable : PlayableBehaviour {
+        public double loopTime;
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
+            var time = playable.GetTime();
+            var count = time + info.deltaTime;
+            var duration = playable.GetDuration();
+            var director = playerData as PlayableDirector;
+            if (director == null) {
+                return;
+            }
+            if (count > duration || Mathf.Approximately((float) time, (float) duration)) {
+                director.time = loopTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GMTK/Master/LoopRegionPlayableAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GMTK.Master {
+    public class LoopRegionPlayableAsset : PlayableAsset {
+        public double loopTime;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
+            var p = ScriptPlayable<LoopRegionPlayable>.Create(graph);
+            var playable = p.GetBehaviour();
+            playable.loopTime = loopTime;
+            return p;
+        }
+    }
+}
