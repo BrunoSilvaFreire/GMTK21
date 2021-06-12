f207ba156919b6966a61315196c7beaeb17ec0e4
Bruno Silva Freire <brunorbsf@gmail.com>
2021-06-12 18:42:28 -0400

    > Animating particles

--- Assets/Scripts/GMTK/Game/Traits/Neutron.cs
+++ Assets/Scripts/GMTK/Game/Traits/Neutron.cs
@@ -12,24 +12,30 @@ namespace GMTK.Game.Traits {
     public class Neutron : Particle {
         public static readonly List<Neutron> AllNeutrons = new List<Neutron>();
 
-        [SerializeField, Range(0f, 1f)] private float randomDirectionTendency = 0.4f;
+        [SerializeField, Range(0f, 1f)]
+        private float randomDirectionTendency = 0.4f;
+
         public float lifeTime = 5f;
-        
+        public bool immortal;
+
         private void OnEnable() {
             AllNeutrons.Add(this);
         }
+
         private void Start() {
-            var attractor = Player.Instance.Pawn.GetTrait<NeutronAttractor>(); 
+            var attractor = Player.Instance.Pawn.GetTrait<NeutronAttractor>();
             attractor.onAttractDirection.AddListener(OnAttractDirection);
             attractor.onAttractPosition.AddListener(OnAttractPosition);
 
-            transform.DOPunchScale(Vector3.one * 0.1f, 0.1f).OnComplete(() => {
-                transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => {
-                    if (AllNeutrons.Count > 1) {
-                        Destroy(gameObject);    
-                    }
-                });
-            }).SetDelay(lifeTime);
+            if (!immortal) {
+                transform.DOPunchScale(Vector3.one * 0.1f, 0.1f).OnComplete(() => {
+                    transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => {
+                        if (AllNeutrons.Count > 1) {
+                            Destroy(gameObject);
+                        }
+                    });
+                }).SetDelay(lifeTime);
+            }
         }
 
         private void OnDestroy() {
--- Assets/Scripts/GMTK/Game/Traits/Particle.cs
+++ Assets/Scripts/GMTK/Game/Traits/Particle.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 
 namespace GMTK.Game.Traits {
     public class Particle : Trait {
+        private const string SpawnedParameter = "Spawned";
+        private const string DiedParameter = "Died";
 
         private Rigidbody rb;
+        public Animator animator;
+        private static readonly int Spawned = Animator.StringToHash(SpawnedParameter);
 
         public new Rigidbody rigidbody => rb;
 
         public override void Configure(TraitDescriptor descriptor) {
             descriptor.RequiresComponent(out rb);
             descriptor.RequiresComponent<Collider>();
+            descriptor.RequiresComponent(out animator);
+            descriptor.RequiresAnimatorParameter(SpawnedParameter, AnimatorControllerParameterType.Trigger);
+            descriptor.RequiresAnimatorParameter(DiedParameter, AnimatorControllerParameterType.Trigger);
         }
 
         private void Start() {
-            var targetScale = transform.localScale;
-            transform.localScale = Vector3.zero;
-            transform.DOScale(targetScale, 0.2f).SetEase(Ease.OutElastic);
-        }
-
-        private void OnDestroy() {
-            transform.DOKill();
+            if (animator) {
+                animator.SetTrigger(Spawned);
+            }
         }
     }
 }
