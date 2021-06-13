9d5112b293f0f3e83457b3764865eae558bfd6f1
Bruno Silva Freire <brunorbsf@gmail.com>
2021-06-13 10:17:04 -0400

    > Importing animations

--- Assets/Scripts/GMTK/Master/LoopRegionPlayableAsset.cs
+++ Assets/Scripts/GMTK/Master/LoopRegionPlayableAsset.cs
@@ -4,11 +4,12 @@ using UnityEngine.Playables;
 namespace GMTK.Master {
     public class LoopRegionPlayableAsset : PlayableAsset {
         public double loopTime;
-
+        public int phaseIndex;
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
             var p = ScriptPlayable<LoopRegionPlayable>.Create(graph);
             var playable = p.GetBehaviour();
             playable.loopTime = loopTime;
+            playable.phaseIndex = phaseIndex;
             return p;
         }
     }
